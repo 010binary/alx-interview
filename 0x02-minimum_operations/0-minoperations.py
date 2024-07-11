f7a785a633c1a3128110c5b06288995046b7bfe5
010binary <augustine32fg@gmail.com>
2024-07-11 21:05:43 +0000

    I don tire

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,17 +7,14 @@ def minOperations(n: int) -> int:
     getting the min operation
     to copy all and paste H
     """
+
+    operations: int = 0
+    divisor: int = 2
     if n <= 1:
         return 0
-    
-    operations = 0
-    divisor = 2
-    
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
     return operations
-
