8664492623a082b70bae4992654ff672e32fa4e2
010binary <augustine32fg@gmail.com>
2024-07-11 21:10:38 +0000

    I don tire

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-""" Module for 0-minoperations"""
+"""
+modula code
+"""
 
-
-def minOperations(n: int) -> int:
-    """
-    getting the min operation
-    to copy all and paste H
-    """
-
-    operations: int = 0
-    divisor: int = 2
-    if n <= 1:
-        return 0
+def minOperations(n):
+    num_ops = 0
+    min_ops = 2
     while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-    return operations
+        while n % min_ops == 0:
+            num_ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return num_ops
