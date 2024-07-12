4555715b66d4f22c93fb3e245f8b85be1d406b5e
010binary <augustine32fg@gmail.com>
2024-07-12 07:40:06 +0000

    trying to fix the pep8 error

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,6 @@
 Module for 0-minoperations
 """
 
-
 def minOperations(n: int) -> int:
     """
     getting the min operation
@@ -12,11 +11,15 @@ def minOperations(n: int) -> int:
 
     operations: int = 0
     divisor: int = 2
+
     if n <= 1:
         return operations
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
+
     return operations
+
