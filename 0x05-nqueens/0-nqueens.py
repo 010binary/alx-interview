9a5ff4f424791994be8e5bcbb9d19cf137544850
010binary <augustine32fg@gmail.com>
2024-08-02 03:47:11 +0100

    n queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,59 +1,59 @@
 #!/usr/bin/python3
-"""Solving the N queens problem"""
+""" N queens """
 import sys
 
 
-def print_board(board, n):
-    """Print the board"""
-    base_case = []
-
-    for x in range(n):
-        for y in range(n):
-            if y == board[x]:
-                b.append([x, y])
-    print(base_case)
-
-
-def safe_position(board, x, y, z):
-    """safe_position to check if a position is safe"""
-    return board[x] in (y, y - x + z, x - z + y)
-
-
-def determine_positions(board, row, n):
-    """determine_positions to determine the positions"""
-    if row == n:
-        print_board(board, n)
-
-    else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if safe_position(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
-                determine_positions(board, row + 1, n)
-
-
-def create_board(size):
-    """create_board to create the board"""
-    return [0 * size for i in range(size)]
-
-
-if len(sys.argv) != 2:
+if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
     exit(1)
 
-try:
-    n = int(sys.argv[1])
-except BaseException:
+if not sys.argv[1].isdigit():
     print("N must be a number")
     exit(1)
 
-if (n < 4):
+if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
 
-board = create_board(int(n))
-row = 0
-determine_positions(board, row, int(n))
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+        i (int, optional): _description_. Defaults to 0.
+        a (list, optional): _description_. Defaults to [].
+        b (list, optional): _description_. Defaults to [].
+        c (list, optional): _description_. Defaults to [].
+
+    Yields:
+        _type_: _description_
+    """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+    """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
