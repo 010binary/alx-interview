df17c0cfd72187e637a012c8abf6c7964c0bfdd2
010binary <augustine32fg@gmail.com>
2024-08-23 02:15:34 +0100

    testing

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,20 +2,25 @@
 """ fewest number of coins needed """
 
 
-def makeChange(coins, total):
-    """
+def makeChange(coins:list, total:int) -> int:
+    """ fewest number of coins needed
 
+    Args:
+        coins (List): list of the values of the coins
+        total (int): the total amount
 
+    Returns:
+        int: return the fewest number of coins needed to meet total
     """
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)  # Sort coins in descending order
+    coins.sort(reverse=True)
     coin_count = 0
     for coin in coins:
         if total == 0:
             break
-        count = total // coin  # Max number of this coin we can use
+        count = total // coin
         coin_count += count
         total -= count * coin
 
